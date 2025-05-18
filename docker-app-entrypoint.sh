78414bea2867148f999c01048927360fddddc796
aisuvro <2842856+aisuvro@users.noreply.github.com>
2025-05-18 23:06:39 +0600

    Add environment configuration, CI/CD workflows, and Kubernetes deployment files

--- docker-app-entrypoint.sh
+++ docker-app-entrypoint.sh
@@ -2,10 +2,10 @@
 
 # Set correct permissions
 chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
-chmod -R 755 /var/www/html/storage /var/www/html/bootstrap/cache
+chmod -R 755 /var/www/html/storage /var/www/html/bootstrap/cache /var/www/html/database /var/www/html/public
 php artisan migrate --force
 chown www-data:www-data database.sqlite
-chmod 664 database.sqlite
+chmod 666 database.sqlite
 
 
 # Start PHP-FPM (this replaces the script process)
